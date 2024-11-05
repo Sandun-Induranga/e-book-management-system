f1b419f7a6a4ad85f9c60aeace78632ecc99fe2a
Sandun Induranga <sandutharu40@gmail.com>
2024-11-06 00:27:59 +0530

    Add project files.

---
+++ EBookPvtLtd/Areas/Identity/Data/EBookPvtLtdContext.cs
@@ -0,0 +1,32 @@
+﻿using EBookPvtLtd.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EBookPvtLtd.Data;
+
+public class EBookPvtLtdContext : IdentityDbContext<Users>
+{
+    public EBookPvtLtdContext(DbContextOptions<EBookPvtLtdContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new UserEntiryConfiguration());
+    }
+}
+
+public class UserEntiryConfiguration : IEntityTypeConfiguration<Users>
+{
+    public void Configure(EntityTypeBuilder<Users> builder)
+    {
+        builder.Property(u => u.Role).IsRequired();
+    }
+}
---
+++ EBookPvtLtd/Areas/Identity/Data/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace EBookPvtLtd.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Users class
+public class Users : IdentityUser
+{
+    public string Role { get; set; } = "Customer";
+}
+
